ec49e8c656ce2f48c8368c1fc0114ce2a5dace14
aceved0coder <112910193+aceved0coder@users.noreply.github.com>
2023-03-16 23:29:11 -0300

    Add files via upload

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -30,7 +30,7 @@ export const ItemListContainer = ()=>{
 
     return(
         <div className="item-list-container">
-            <p></p>
+            <p className="titulo-store">GET AWAY CAR</p>
             <ItemList items={productos}/>
         </div>
     )
